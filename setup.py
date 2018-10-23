327675ff9f88aaac8a6998e87162d25690d41fbe
Matthias De Smet <matthias.desmet@ugent.be>
2018-10-23 14:13:15 +0200

    setup: v1.0.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 from setuptools import setup, find_packages
 
-version = '1.0.0-dev'
+version = '1.0.1'
 dl_version = 'master' if 'dev' in version else '{}'.format(version)
 
 setup(
