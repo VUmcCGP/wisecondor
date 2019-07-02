68a044bc1aaf38b7bdc2c4959acb6a332cd2a5bc
Lennart <leraman@arraycgh.ugent.be>
2019-07-02 15:30:08 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 from setuptools import setup, find_packages
 
-version = '1.1.1'
+version = '1.1.2'
 dl_version = 'master' if 'dev' in version else '{}'.format(version)
 
 setup(
